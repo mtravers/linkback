caf352908a72553038acb8f6856ec0677981c482
Mike Travers <mt@hyperphor.com>
2012-06-15 08:56:03 -0700

    pkg prob sigh

--- src/heroku.lisp
+++ src/heroku.lisp
@@ -1,10 +1,10 @@
-(in-package :net.aserve)
+(in-package :wu)
 
 ;;; Called at application initialization time.
 (defun cl-user::initialize-application ()
   (setq *access-id* (ccl:getenv "SEOMOZ_ACCESS_ID"))
   (setq *secret* (ccl:getenv "SEOMOZ_SECRET"))
-  (wu:wuwei-initialize-application :directory "/app/wupub/")) ;for some reason ./wupub is failing, try this.
+  (wuwei-initialize-application :directory "/app/wupub/")) ;for some reason ./wupub is failing, try this.
 
 
 
