7b98456c1622894a1c748637497dea60299f5b19
Mike Travers <mt@hyperphor.com>
2012-06-15 08:42:16 -0700

    try to solve yet another config problem

--- src/heroku.lisp
+++ src/heroku.lisp
@@ -4,7 +4,7 @@
 (defun cl-user::initialize-application ()
   (setq *access-id* (ccl:getenv "SEOMOZ_ACCESS_ID"))
   (setq *secret* (ccl:getenv "SEOMOZ_SECRET"))
-  (wu:wuwei-initialize-application))
+  (wu:wuwei-initialize-application :directory "/app/wupub/")) ;for some reason ./wupub is failing, try this.
 
 
 
