18f5786c3187e943dbf76dbf66959899d1b601e8
Mike Travers <mt@hyperphor.com>
2012-07-08 12:08:39 -0700

    fix some css issues

--- extension/script.js
+++ extension/script.js
@@ -220,6 +220,7 @@ function opencloseUpdate() {
 function addStyleLink(href) {
     var head, link;
     head = document.getElementsByTagName('head')[0];
+    if (!head) { head = document.getElementsByTagName('body')[0]; } // headless body found in topless bar
     if (!head) { return; }
     link = document.createElement('link');
     link.type = 'text/css';
